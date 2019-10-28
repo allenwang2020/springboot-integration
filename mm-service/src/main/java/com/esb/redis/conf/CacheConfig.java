b93125985bce9ab0aa8810a6d1283d43e6663800
allenwang2020 <allenwang2020@gmail.com>
2019-10-28 10:36:58 +0800

    增加redisUtil及CacheConfig

--- mm-service/src/main/java/com/esb/redis/conf/CacheConfig.java
+++ mm-service/src/main/java/com/esb/redis/conf/CacheConfig.java
@@ -4,12 +4,12 @@ import java.time.Duration;
 
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -20,59 +20,39 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableCaching //啟用緩存
 public class CacheConfig extends CachingConfigurerSupport {
 
-	    // 自定义key生成器
-	    @Bean
-	    public KeyGenerator keyGenerator(){
-	        return (o, method, params) ->{
-	            StringBuilder sb = new StringBuilder();
-	            sb.append(o.getClass().getName()); // 类目
-	            sb.append(method.getName()); // 方法名
-	            for(Object param: params){
-	                sb.append(param.toString()); // 参数名
-	            }
-	            return sb.toString();
-	        };
-	    }
-
-	    // 配置缓存管理器
-	    @Bean
-	    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-	        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-	                .entryTtl(Duration.ofSeconds(60)) // 60s缓存失效
-	                // 设置key的序列化方式
-	                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
-	                // 设置value的序列化方式
-	                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer()))
-	                // 不缓存null值
-	                .disableCachingNullValues();
-
-	        RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory)
-	                .cacheDefaults(config)
-	                .transactionAware()
-	                .build();
-
-	        return redisCacheManager;
-	    }
-
-	  /*  @Bean
-	    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory){
-	        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-	        redisTemplate.setConnectionFactory(connectionFactory);
-	        redisTemplate.setKeySerializer(keySerializer());
-	        redisTemplate.setHashKeySerializer(keySerializer());
-	        redisTemplate.setValueSerializer(valueSerializer());
-	        redisTemplate.setHashValueSerializer(valueSerializer());
-	        logger.info("序列化完成！");
-	        return redisTemplate;
-	    }*/
-
-	    // key键序列化方式
-	    private RedisSerializer<String> keySerializer() {
-	        return new StringRedisSerializer();
-	    }
-
-	    // value值序列化方式
-	    private GenericJackson2JsonRedisSerializer valueSerializer(){
-	        return new GenericJackson2JsonRedisSerializer();
-	    }
+	//過期時間1天
+    private Duration timeToLive = Duration.ofDays(1);
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        //預設1
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(this.timeToLive)
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer()))
+                .disableCachingNullValues();
+        RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .transactionAware()
+                .build();
+       // log.debug("自定義RedisCacheManager加载完成");
+        return redisCacheManager;
+    }
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(keySerializer());
+        redisTemplate.setHashKeySerializer(keySerializer());
+        redisTemplate.setValueSerializer(valueSerializer());
+        redisTemplate.setHashValueSerializer(valueSerializer());
+       // log.debug("自定義RedisTemplate加载完成");
+        return redisTemplate;
+    }
+    private RedisSerializer<String> keySerializer() {
+        return new StringRedisSerializer();
+    }
+    private RedisSerializer<Object> valueSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+	 
 }
